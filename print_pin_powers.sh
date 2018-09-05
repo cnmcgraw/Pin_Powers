92ea896b0fd3a2c309a8be1b174f4b30aa0c32a7
Carolyn Mcgraw <mcgraw6@quartz380.llnl.gov>
2018-09-05 13:55:29 -0700

    simplifying

--- print_pin_powers.sh
+++ print_pin_powers.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 filename="$1"
 echo $filename
-while read -r line
-do
-    if(line=='[0]              Pin Power Map (MeV/s)')
-    then
-      for i in `seq 1 16`;
-      do
-          num=3
-          j=$(($i+$num))
-          grep -A$j 'Pin Power Map (MeV/s)' $filename | tail -n1 >> $2
-      done
-    fi
-done < "$filename"
+grep -A19 'Pin Power Map (MeV/s)' $filename >> $2
 
