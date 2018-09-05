9df7e425103cad89bbf2119346565dafdd95ca51
Carolyn Mcgraw <mcgraw6@quartz380.llnl.gov>
2018-09-05 15:10:44 -0700

    broken

--- print_pin_powers.sh
+++ print_pin_powers.sh
@@ -4,14 +4,12 @@ for a in `seq 2 19`;
 do
   line=$(grep -A$a 'Pin Power Map (MeV/s)' $filename)
   line=${line##*]}
- # echo $line
   num=21
   b=$(($a + $num))
   newline=$(grep -A$b 'Pin Power Map (MeV/s)' $filename)
   newline=${newline##*]}
-  newline=${newline:1} 
- # echo $newline
-  newnewline=$(($line + $newline))
+  newline=${newline:1:16}
+  newnewline=$line$newline
   echo $newnewline >> $2
 done
 
