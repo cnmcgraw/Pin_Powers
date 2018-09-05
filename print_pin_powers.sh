35312bee71ee310ffc85285c743fec4e5be1da04
Carolyn Mcgraw <mcgraw6@quartz380.llnl.gov>
2018-09-05 15:23:10 -0700

    finally fixed

--- print_pin_powers.sh
+++ print_pin_powers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 filename="$1"
-for a in `seq 2 19`;
+for a in `seq 3 19`;
 do
   line=$(grep -A$a 'Pin Power Map (MeV/s)' $filename)
   line=${line##*]}
@@ -8,7 +8,7 @@ do
   b=$(($a + $num))
   newline=$(grep -A$b 'Pin Power Map (MeV/s)' $filename)
   newline=${newline##*]}
-  newline=${newline:1:16}
+  newline=$(echo "$newline" | cut -c 17-)
   newnewline=$line$newline
   echo $newnewline >> $2
 done
